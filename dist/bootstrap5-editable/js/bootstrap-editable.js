4982f15d2cfa114bba124cde9973203998027f61
Tinh Nguyen <tinh.nguyen@mud-maps.com>
2023-02-25 21:07:35 +0700

    use bs5 icon classes

--- dist/bootstrap5-editable/js/bootstrap-editable.js
+++ dist/bootstrap5-editable/js/bootstrap-editable.js
@@ -4753,10 +4753,10 @@ Editableform based on Twitter Bootstrap 3
     //buttons
     $.fn.editableform.buttons = 
       '<button type="submit" class="btn btn-primary btn-sm editable-submit">'+
-        '<i class="fa fa-check" aria-hidden="true"></i>'+
+        '<i class="bi bi-check" aria-hidden="true"></i>'+
       '</button>'+
-      '<button type="button" class="btn btn-default btn-sm editable-cancel">'+
-        '<i class="fa fa-times" aria-hidden="true"></i>'+
+      '<button type="button" class="btn btn-danger btn-sm editable-cancel">'+
+        '<i class="bi bi-x" aria-hidden="true"></i>'+
       '</button>';         
     
     //error classes
